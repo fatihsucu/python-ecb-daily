9144b1aa58687fb5cb9109f0408143350810f523
Fatih Sucu <fatihsucu@Fatihs-MacBook-Air.local>
2021-04-12 14:09:46 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='python-ecb-daily',
+      version='0.1',
+      description='Python ECB Daily Rates Wrapper',
+      url='https://github.com/fatihsucu/python-ecb-daily',
+      author='Fatih Sucu',
+      author_email='fatihsucu0@gmail.com',
+      license='MIT',
+      packages=['ecb'],
+      zip_safe=False
+)
